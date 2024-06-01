799f9a4d4838faadc386833c174c8a9ecd362961
Gift <aghaulor.gift@gmail.com>
2024-06-01 07:50:45 -0700

    2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  const selectedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+
+    const amenityList = Object.values(selectedAmenities).join(', ');
+    $('.amenities h4').text('Amenities: ' + amenityList);
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
